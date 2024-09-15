3fba20b62937cbab916270cd6f9199044f33d0e7
moniquebritto21 <moniquencbritto@gmail.com>
2024-09-15 20:51:59 +0000

    flask run works now edited __init__.py

--- App/__init__.py
+++ App/__init__.py
@@ -1,4 +1,38 @@
-from .models import *
-from .views import *
-from .controllers import *
-from .main import *
+# App/__init__.py
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# Initialize the database
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+
+    # Add your app configurations
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
+    
+    # Initialize the database with the app
+    db.init_app(app)
+
+    # Import models, views, etc., after app is created
+    from .models import user
+    from .views import some_view
+
+    # Register Blueprints or other app components
+    # app.register_blueprint(some_blueprint)
+
+    return app
+
+
+
+
+
+
+
+
+
+
+# from .models import *
+# from .views import *
+# from .controllers import *
+# from .main import *
